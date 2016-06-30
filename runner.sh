d7364d0150fe5c6a310adecfc0aa92288fbc2322
Ben Johnson <ben@gophronesis.com>
2016-06-30 09:27:08 -0400

    moving files around

--- runner.sh
+++ runner.sh
@@ -1,43 +1,21 @@
 # --
-# Development dataset 
-
-DATA=data/part-00000
-
-# Compute actual locations
-redis-cli flushall
-cat $DATA | parallel -u --pipe -N 100000 -P 10 "python run-graph.py"
-cat $DATA | python run-predict.py > output/act
-
-# Compute predicted locations
-redis-cli flushall
-cat $DATA | parallel -u --pipe -N 100000 -P 6 "python run-graph.py"
-cat $DATA | python run-predict.py --always-predict > output/pred-n3-mad30-speedinf
-
-python munge-results.py
-
-
-# --
 # On server
 
 mkdir -p output
 
-DATA=data/
 PCOM="parallel -u --pipe -N 100000 -P 10"
 
 # Compute actual locations
 redis-cli flushall
-time cat data/run1-100.gz | gzip -cd | parallel -u --pipe -N 100000 -P 10 "python run-graph.py"
-time cat data/run1-100.gz | gzip -cd | python run-predict.py > output/act
+time cat data/run1-100.gz | gzip -cd | parallel -u --pipe -N 100000 -P 10 "python pipe-graph.py"
+time cat data/run1-100.gz | gzip -cd | python pipe-predict.py > output/act
 
 # Compute predicted locations
 redis-cli flushall
-
-time cat data/run1-100.gz | gzip -cd | $PCOM "python run-graph.py"
+time cat data/run1-100.gz | gzip -cd | $PCOM "python pipe-graph.py"
 
 # real    7m10.003s
 # user    66m19.108s
 # sys     2m53.220s
 
-time cat data/run1-100.gz | gzip -cd | python run-predict.py --always-predict > output/pred
-
-python munge-results.py
+time cat data/run1-100.gz | gzip -cd | python pipe-predict.py --always-predict > output/pred
